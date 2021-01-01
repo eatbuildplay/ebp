49f219c3d3171b85d8a65412da80d74f4c7f4e12
Casey Milne <casey.milne@eatbuildplay.com>
2021-01-01 14:42:04 -0500

    update gitignore

--- uploads/sucuri/sucuri-hookdata.php
+++ uploads/sucuri/sucuri-hookdata.php
@@ -1,6 +1,6 @@
 <?php
 // datastore=hookdata;
 // created_on=1607574976;
-// updated_on=1609528143;
+// updated_on=1609529826;
 exit(0);
 ?>
--- uploads/sucuri/sucuri-lastlogins.php
+++ uploads/sucuri/sucuri-lastlogins.php
@@ -4,3 +4,4 @@
 {"user_id":1,"user_login":"admin","user_remoteaddr":"127.0.0.1","user_hostname":"LAPTOP-Q4AFMNDQ","user_lastlogin":"2020-12-27 17:02:13"}
 {"user_id":1,"user_login":"admin","user_remoteaddr":"127.0.0.1","user_hostname":"LAPTOP-Q4AFMNDQ","user_lastlogin":"2020-12-27 17:43:22"}
 {"user_id":1,"user_login":"admin","user_remoteaddr":"127.0.0.1","user_hostname":"LAPTOP-Q4AFMNDQ","user_lastlogin":"2020-12-27 17:45:04"}
+{"user_id":1,"user_login":"admin","user_remoteaddr":"127.0.0.1","user_hostname":"LAPTOP-Q4AFMNDQ","user_lastlogin":"2021-01-01 19:35:39"}
